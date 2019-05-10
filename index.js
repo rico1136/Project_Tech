d314a6ade4bf983115b10efb51d7f9d6295c2ed0
Joan <Joanpadolina@gmail.com>
2019-05-10 16:29:30 +0200

    verwijder node_modules

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 
 const express = require("express"); // https://expressjs.com/
+const app = express();
+const server = app.listen(3000);
+app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.send('try this out')
+})
+
+
 const camelCase = require("camelcase"); // test package installed
 console.log(camelCase('werkt-dit-of-totaal-niet3'));
 
