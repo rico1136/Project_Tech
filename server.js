1f49d3810100648382bbe55a93ae42bf8ce3f58a
Joan <Joanpadolina@gmail.com>
2019-05-14 17:53:18 +0200

    Trying out templating

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-const camelCase = require("camelcase"); // test package installed
+const camelCase = require('camelcase'); // test package installed
 console.log(camelCase('werkt-dit-of-totaal-niet3'));
 
-const ejs = require("ejs");
-
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
+
+// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
+// app.use("/route", express.static("foldername"));
+app.use('/public', express.static('public'));
+
+//index.ejs package
+
+app.get('/', function (req, res){
+  res.render('pages/index');
+});
+
+//profile page
+
+app.get('/profile', function(req, res){
+  res.render('pages/profile');
+});
+
+
 app.get('/', function(request, response) {
-   response.send('veranderen')
+   response.send('red')
 });
 
 app.listen(port, function() {
