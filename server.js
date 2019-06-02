ad1333587b433b72cb1d385b6c4163b7413489fd
Joan <Joanpadolina@gmail.com>
2019-06-02 17:36:31 +0200

    Server.js more readable + looping feed mongodb

--- server.js
+++ server.js
@@ -9,38 +9,11 @@ const multer = require('multer');          //https://www.npmjs.com/package/multe
 const arrayFind = require('array-find');   //https://www.npmjs.com/package/array-find
 const mongo = require('mongodb');          //https://www.mongodb.com/
 const mongoose = require('mongoose');      //https://www.npmjs.com/package/mongoose
+const session = require('express-session'); //https://www.npmjs.com/package/express-session
+require('dotenv').config(); // gegeven voor de mongodb server
 
-// ---- opslaan van foto's in deze map --//
-const upload = multer({
-  dest: 'public/upload/'
-});
+// == data accounts array == //
 
-require('dotenv').config();
-
-mongoose
-  .connect(
-    'mongodb://localhost:27017/auth', {
-      useNewUrlParser: true
-    }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log('Mongo Error:'));
-
-var db = null;
-var url = 'mongodb://localhost:27017/Project_Tech' + process.env.DB_HOST + ':' + process.env.DB_PORT;
-
-mongo.MongoClient.connect(url, {
-  useNewUrlParser: true
-}, function(err, client) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('You now have access to ' + url);
-  }
-  db = client.db(process.env.DB_NAME)
-})
-
-// -- Data array -- //
 let accounts = [{
     id: 1,
     name: 'joan',
@@ -59,120 +32,169 @@ let accounts = [{
     email: 'janno@hotmail.com',
     password: '4321',
     file: ''
+  },
+  {
+    id: 3,
+    name: 'jess',
+    age: '27',
+    state: 'man',
+    email: 'janno@hotmail.com',
+    password: '4321',
+    file: ''
   }
 ];
 
+// foto's opslaan in een map //
+const upload = multer({dest:'public/upload'});
 
-// -- templating engine EJS -- //
+// ---- CMD-BT Slides MongoDB ---//
+
+var db = null;
+var url = 'mongodb://localhost:27017/Project_Tech' + process.env.DB_HOST + ':' + process.env.DB_PORT;
+
+mongo.MongoClient.connect(url, {
+  useNewUrlParser: true
+}, function(err, client) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('You now have access to ' + url);
+  }
+  db = client.db(process.env.DB_NAME)
+})
+
+// express engine //
 app.set('view engine', 'ejs');
 
-// -- statische website --//
-app.use('/public', express.static('public'));
+// routing van de pagina's //
 
-// -- formlezer en slaat het op in een js code --/
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.get('/', index);
+app.get('/login', login);
+app.get('/profile' + '/:id', findProfile);
+app.get('/profile', ownProfile);
+app.get('/list', listPage);
+app.get('/feed', feedList)
+
+app.get('/register', register);
+
+app.post('/register', upload.single('file'), addRegis);
+app.post('/:id', addRegis);
+
+app.use(express.static('public'));
+// leest de form en slaat het op in een js code
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(errNotFound);
 
 
-app.get('/', function(req, res) {
+
+app.listen(port, servermsg);
+
+
+
+//--- pagina render---//
+
+function index(req, res){
   res.render('pages/index');
-});
+}
 
-// --------- Pagina's ------------//
-app.get('/', function(req, res) {
-  res.render('/pages/index')
-});
-
-app.get('/list', function(req, res) {
-  res.render('pages/list');
-});
-
-
-app.get('/register', function(req, res) {
+function register(req, res, next){  
   res.render('pages/register');
-});
+}
+
+function login(req, res){
+  res.render('pages/login');
+}
+
+function ownProfile(req, res){
+  res.render('pages/profile');
+}
+
+function listPage(req, res){
+  res.render('pages/list');
+}
+function matchFeed(req, res){
+  res.render('pages/feed');
+}
+
+function dbCollect(req, res, next){ // require, response, alles tussen de req en res (middleware)
+  db.collecction('account');
+}
 
 
-//-- opgeslagen ID's ophalen --//
+// mongodb 
 
-app.get('/:id', function(req, res) { // Rico 
-  let obj;
-  obj = accounts.find(obj => obj.id == req.params.id);
-  res.render('pages/profile.ejs', {
-    title: `Profile of ${obj.name}`,
-    obj: obj
-  });
-  console.log(id);
-});
-
-// --- Register form posten ---// 
-
-app.post('/register', upload.single('file'), function(req, res, next) {
-  
-// ---- account toevoegen aan collectie moongo Compass ----//
-  
-  db.collection('account').insertOne({
-
-      name: req.body.name,
-      age: req.body.age,
-      state: req.body.state,
-      email: req.body.email,
-      file: req.file ? req.file.filename : null, // if else
+function findProfile (req, res, next){
+  let id = req.params.id
+  db.collection('account').findOne({
+    _id: new mongo.ObjectID(id)
   }, done)
-
-  function done(err, data ) {
-    if (err) {
-      console.log(next(err))
-    } else {
-      console.log(id+' is added to the database.');
-      // res.redirect('/' + data.insertedId)
-
+  
+  
+  function done(err, data){
+    if(err){
+      next(err)
+    }else{
+      res.render('pages/profile.ejs', {data:data})
     }
   }
+}
+
+function feedList(req, res, next){
+  db.collection("account").find().toArray(function(err, data) {
+      res.render('pages/feed', {data:data});
+      // res.render(data);
+  });
+  }
   
-// --- Data wordt verzonden naar de EJS template ---//  
-  console.log(req.body.name);
-  let id = slug(req.body.name).toLowerCase();
 
-  accounts.push({
+// function deleteAccount(req, res, next){
+//   let id = req.params.id 
+//   db.collection('account').deleteOne({
+//     _id:mongo.ObjectID(id)
+//   }, done)
+// 
+//   function done(err){
+//     if(err){
+//       next(err)
+//     }else{
+//       res.json({status:'ok'})
+//     }
+//   }
+// }
 
 
-    id: id,
+function addRegis(req,res, next){
+//slugify url friendly
+let id = slug(req.body.name).toLowerCase()
+// ---- account toevoegen aan collectie moongo Compass ----//
+
+db.collection('account').insertOne({
+
     name: req.body.name,
     age: req.body.age,
     state: req.body.state,
     email: req.body.email,
-    password: req.body.password,
     file: req.file ? req.file.filename : null, // if else
+}, done)
 
-  });
-  // 
-  // console.log(req.body);
-  return res.redirect('/'+ id);
-  
+function done(err, data ) {
+  if (err) {
+    console.log(next(err))
+  } else {
+    console.log(id+' is added to the database.');
+    res.redirect('/profile/' + data.insertedId)
+
+  }
+}
+}
 
 
+////////////////////////////////////////////////////
 
+function servermsg(){
+  console.log('De server is geactiveerd!');
+}
 
-});
-
-// --------- 404 ERROR ------------//
-
-app.use(function(req, res) {
+function errNotFound(req, res){
   res.status(404).render('pages/404');
-
-});
-
-//Start Server
-///////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-app.get('/', function(request, response) {
-  response.send('red')
-});
-
-app.listen(port, function() {
-  console.log('De server staan aan!')
-});
+}
