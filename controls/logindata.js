9228ef2fd50b2c5800b9fa46bb2e74930525b1b7
Vince Kaars <vince.kaars@quicknet.nl>
2019-06-14 09:48:00 +0200

    Beautify code

--- controls/logindata.js
+++ controls/logindata.js
@@ -4,10 +4,13 @@ const User = require('../controls/userschema');
 const session = require('express-session'); //https://www.npmjs.com/package/express-session
 
 router.post('/login', function (req, res, next) {
-  User.findOne({email: req.body.email, password: req.body.password}, function (err, currentUser) {
+  User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  }, function (err, currentUser) {
     if (err) {
       next(err)
-    }else {
+    } else {
       req.session.user = currentUser;
       res.redirect(`/profile/${req.session.user._id}`)
     }
