3d0e97dbdc9be8fe4c900b95c4319a4c44c556e5
Coen Mathijssen <mathijssen.coen@gmail.com>
2019-06-14 10:17:11 +0200

    The data from the updated user will now be directly rendered in the updated profile page

--- controls/update.js
+++ controls/update.js
@@ -7,47 +7,47 @@ an object (user) in the databse. If it does, it will take that object and replac
 if they are filled in. Then it will save this new object which will override the existing one. At last, the profile-overview
 page will be rendered again with the freshly updated data.  */
 router.post('/updateFunction', (req, res) => {
-console.log('running');
-const id = req.session.user._id
-User.findOne({ _id: id }, (err, foundObject) => {
-  if (err) {
-    console.log(err)
-    res.status(500).send()
-  } else {
-    if (!foundObject) {
-      console.log('User not found in database')
-      res.status(404).send()
+  console.log('running');
+  const id = req.session.user._id
+  User.findOne({ _id: id }, (err, foundObject) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send()
     } else {
-      console.log('user found: ', foundObject)
-      if (req.body.name) {
-        foundObject.name = req.body.name
-      } if (req.body.age) {
-        foundObject.age = req.body.age
-      } if (req.body.sex) {
-        foundObject.sex = req.body.sex
-      } if (req.body.email) {
-        foundObject.email = req.body.email
-      } if (req.body.password) {
-        foundObject.password = req.body.password
-      } if (req.body.profilePic){
-        foundObject.profilePic = req.body.profilePic
-      }if (req.body.memeCategory) {
-        foundObject.memeCategory = req.body.memeCategory
-      }
+      if (!foundObject) {
+        console.log('User not found in database')
+        res.status(404).send()
+      } else {
+        console.log('user found: ', foundObject)
+        if (req.body.name) {
+          foundObject.name = req.body.name
+        } if (req.body.age) {
+          foundObject.age = req.body.age
+        } if (req.body.sex) {
+          foundObject.sex = req.body.sex
+        } if (req.body.email) {
+          foundObject.email = req.body.email
+        } if (req.body.password) {
+          foundObject.password = req.body.password
+        } if (req.body.profilePic) {
+          foundObject.profilePic = req.body.profilePic
+        } if (req.body.memeCategory) {
+          foundObject.memeCategory = req.body.memeCategory
+        }
         foundObject.save((err, updatedObject) => {
+          console.log('updatedObject: ', updatedObject)
           if (err) {
             console.log(err)
             res.status(500).send()
           } else {
-            console.log('user saved')
             res.render('pages/profile', {
-              user: updatedObject
+              data: updatedObject
             })
           }
         })
       }
     }
- })
+  })
 })
 
 module.exports = router;
