1148144e557163b1949dd11dc341065736e2806b
GiovanniKaaijk <kaaijkg@gmail.com>
2019-06-18 12:24:42 +0200

    Added liking other users, check if they like you back

--- controls/profile.js
+++ controls/profile.js
@@ -25,6 +25,36 @@ router.get('/profile', (req, res) => {
     res.redirect(`/profile/${req.session.user._id}`);
 });
 
+
+router.get('/userprofile/:id', (req, res) => {
+    if (!req.session.user) {
+        res.redirect('/login');
+        return
+    }
+    User.findOne({_id : req.params.id}, (err, profile) => {
+        let like = false;
+        if (err) {
+            throw(err);
+        } else {
+            for(let x = 0;x<req.session.user.likes.length;x++){
+                console.log('loop')
+                if(req.session.user.likes[x] === profile._id ){
+                    console.log('liked by current user')
+                    for(let i = 0;i<profile.likes.length;i++){
+                        if(profile.likes[i] === req.session.user._id) {
+                            console.log('liked by other user')
+                            like = true;
+                        }
+                    }
+                } else {
+                    like = false;
+                }
+            }
+            res.render('pages/userprofile', {data: profile, user: req.session.user, liked: like})
+        }
+    });
+});
+
 module.exports = router;
 
 
