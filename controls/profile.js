1de1d637679c16047e9146ed59fe07695798f051
GiovanniKaaijk <kaaijkg@gmail.com>
2019-06-18 12:35:10 +0200

    Changed userprofile.ejs when both people like each other

--- controls/profile.js
+++ controls/profile.js
@@ -37,11 +37,11 @@ router.get('/userprofile/:id', (req, res) => {
             throw(err);
         } else {
             for(let x = 0;x<req.session.user.likes.length;x++){
-                console.log('loop')
-                if(req.session.user.likes[x] === profile._id ){
+                console.log(req.session.user.likes[x])
+                if(req.session.user.likes[x] == profile._id ){
                     console.log('liked by current user')
                     for(let i = 0;i<profile.likes.length;i++){
-                        if(profile.likes[i] === req.session.user._id) {
+                        if(profile.likes[i] == req.session.user._id) {
                             console.log('liked by other user')
                             like = true;
                         }
